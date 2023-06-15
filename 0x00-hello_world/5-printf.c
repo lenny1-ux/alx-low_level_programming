b31a5922a26aec06a51db336a34f8371c02d1747
lenny1-ux <leonardsammy01@gmail.com>
2023-06-15 16:47:34 -0700

    using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,11 @@
-#include<stdio.h>
-/* print without using puts*/
+#include <stdio.h>
+
+/**
+ *  print without using puts
+ *a string
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return(0);
 }
