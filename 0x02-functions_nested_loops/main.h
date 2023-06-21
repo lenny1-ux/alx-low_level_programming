c8ea2c526eb3e3c7749830fc3e90cb425ab194de
lenny1-ux <leonardsammy01@gmail.com>
2023-06-20 19:57:59 -0700

    alpha-0x

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,10 @@
 #define MAIN_HEADER_FILE
 int _putchar(char c);
 void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
 void print_times_table(int n);
 #endif
