452554a3b2dcec23a70000b55ed87bec92236c5f
lenny1-ux <leonardsammy01@gmail.com>
2023-07-20 19:11:55 -0700

    print sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,27 +1,26 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+
 /**
- * sum_them_all - function that returns the sum of all its parameters.
- * @n: resepresents the number of arguments
- * Return: Always 0.
+ * sum_them_all - Returns the sum of all its paramters.
+ * @n: The number of paramters passed to the function.
+ * @...: A variable number of paramters to calculate the sum of.
+ *
+ * Return: If n == 0 - 0.
+ *         Otherwise - the sum of all parameters.
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	/* creating va_list to store the variable argument list */
-	va_list my_nums;
-	unsigned int count;
-	int sum;
+	va_list ap;
+	unsigned int i, sum = 0;
 
-	if (n == 0)
-		return (0);
-	sum = 0;
+	va_start(ap, n);
 
-	/* init valist for the number of arguments */
-	va_start(my_nums, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
 
-	/* loop through/access all arguments stored in the valist */
-	for (count = 0; count < n; count++)
-		sum += va_arg(my_nums, int);
-	va_end(my_nums);
+	va_end(ap);
+
 	return (sum);
 }
+
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,16 +1,10 @@
-#ifndef VARIADIC_FNS
-#define VARIADIC_FNS
+#ifndef _VARIADIC_H_
+#define _VARIADIC_H_
 
-#define VALIDTYPESCONDITIONAL(i) (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
-
-typedef struct validTypes
-{
-	char *valid;
-	void (*f)();
-} v_types;
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 #endif
+
