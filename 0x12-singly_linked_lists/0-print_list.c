723a442b14c707116e18d68d68ff345d89e7593b
lenny1-ux <leonardsammy01@gmail.com>
2023-07-28 09:46:48 -0700

    linked data structures

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * print_list : prints all the elements of a linked list
+ * pointer to the list_t list to be shown
+ * Return: the number of nodes printed
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	for (i = 0; h; i++)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (i);
+}
