64bb07822b3aff7c13fc98c3a98b37f6adb53837
lenny1-ux <leonardsammy01@gmail.com>
2023-08-09 20:22:15 -0700

    updates

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,6 +4,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
