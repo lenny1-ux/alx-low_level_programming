4e7ceed6e5b9f1cfc626104c5d5082aa67f0ddd7
lenny1-ux <leonardsammy01@gmail.com>
2023-10-21 02:06:00 +0300

    task2 fix

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -g -fPIC *.c 
-gcc -shared -o liball.so
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+gcc -Wall -Werror -Wextra -pedantic -g -fPIC *.c -shared -o liball.so
