238571cc3e6af7672b87cd8bff9a428697584d34
lenny1-ux <leonardsammy01@gmail.com>
2023-10-21 08:03:06 -0700

    fix betty

--- 0x18-dynamic_libraries/len.c
+++ 0x18-dynamic_libraries/len.c
@@ -1,8 +1,13 @@
+/**
+ * rand- the loop for the function in array format
+ * Return: randy value
+ */
+
 int rand(void)
 {
 	static int i = -1;
 	int randy[6] = {8, 8, 7, 9, 23, 74};
 
 	i++;
-	return(randy[i]);
+	return (randy[i]);
 }
--- 0x18-dynamic_libraries/replace.c
+++ 0x18-dynamic_libraries/replace.c
@@ -1,1 +1,5 @@
-main(){write(1,"9 8 10 24 75 - 9\nCongratulations, you win the Jackpot!\n",55);return(0);}
+main()
+{
+	write(1, "9 8 10 24 75 - 9\nCongratulations, you win the Jackpot!\n", 55);
+	return (0);
+}
