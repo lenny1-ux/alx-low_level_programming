e0d2cadaa3ce098f3c8a6df5ff553fb8866f6cc9
lenny1-ux <leonardsammy01@gmail.com>
2023-10-21 04:52:05 -0700

    jackpot hack

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -g -fPIC *.c 
-gcc -shared -o liball.so
+gcc -Wall -Werror -Wextra -pedantic -g -fPIC *.c -shared -o liball.so
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
