4e90b657648f61b46ab4f005bae6459d4a9962f1
lenny1-ux <leonardsammy01@gmail.com>
2023-10-21 06:39:36 -0700

    fix bugs

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O  https://github.com/lenny1-ux/alx-low_level_programming/blob/main/0x18-dynamic_libraries/liblen.so
+wget -q  https://github.com/lenny1-ux/alx-low_level_programming/blob/main/0x18-dynamic_libraries/liblen.so
 export LD_PRELOAD=/$PWD/liblen.so
